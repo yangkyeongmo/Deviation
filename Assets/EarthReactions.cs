2404227759294d06933dbfb44b160da6ff879702
yangkyeongmo <yangkyeongmo@snu.ac.kr>
2018-01-08 00:11:12 +0900

    모행성에서 플레이어에게 유도미사일 3초마다 발사

---
+++ Assets/EarthReactions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EarthReactions : MonoBehaviour {
+
+    public GameObject followingShot;
+
+    private Vector3 radialSpawnPosition;
+    private int firstValue;
+    private int nextValue = 0;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        firstValue = (int)System.Math.Truncate(Time.time / 1);
+
+        if (firstValue == nextValue)
+        {
+            radialSpawnPosition = CircularToCartesianCoordinate(transform.localScale.x + 2.0f, Random.Range(0.0f, 360.0f * Mathf.Deg2Rad));
+            Instantiate(followingShot, radialSpawnPosition, Quaternion.identity);
+            nextValue = firstValue + 3;
+        }
+	}
+    
+    Vector3 CircularToCartesianCoordinate(float rad, float angle)
+    {
+        Vector3 circToCart;
+        circToCart.x = rad * Mathf.Cos(angle);
+        circToCart.y = rad * Mathf.Sin(angle);
+        circToCart.z = 0;
+        return circToCart;
+    }
+}
---
+++ Assets/MoveFollowingShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFollowingShot : MonoBehaviour {
+
+    public float shotSpeed;
+
+    private GameObject playerObject;
+    private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        playerObject = GameObject.Find("Satellite0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direction = (playerObject.transform.position - transform.position).normalized;
+        transform.position += direction * shotSpeed * Time.deltaTime;
+        transform.up = direction;
+	}
+}
